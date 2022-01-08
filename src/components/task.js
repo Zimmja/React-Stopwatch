73a8bd591fb96a4552a3d5ad7bbb4f2a8deb1347
Matt Zimmer <zimmermja@gmail.com>
2022-01-08 16:40:31 +0000

    Added a delete button

--- src/components/task.js
+++ src/components/task.js
@@ -3,23 +3,21 @@ import "./task.css";
 import Stopwatch from "./stopwatch.js";
 
 export default class Task extends React.Component {
-  handleClick = () => this.props.onClick(this.props.index);
-
-  hours = () => this.fTime(parseInt(this.props.timer / 3600000) % 24);
-  minutes = () => this.fTime(parseInt(this.props.timer / 60000) % 60);
-  seconds = () => this.fTime(parseInt(this.props.timer / 1000) % 60);
-  fTime = (time) => (time < 10 ? `0${time}` : `${time}`);
+  handleSWClick = () => this.props.onSWClick(this.props.index);
+  handleDClick = () => this.props.onDClick(this.props.index);
 
   render() {
     return (
       <div className="task">
-        <button className="stopwatch" onClick={() => this.handleClick()}>
+        <button className="stopwatch" onClick={() => this.handleSWClick()}>
           <Stopwatch
             key={`stopwatch${this.props.index}`}
             timer={this.props.timer}
             index={this.props.index}
-            onClick={(i) => this.selectTask(i)}
           />
+        </button>
+        <button className="deleteButton" onClick={() => this.handleDClick()}>
+          Delete
         </button>
       </div>
     );
