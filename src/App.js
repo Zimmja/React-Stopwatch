e46bd6bd1390446c07ec7ec9030619c3b15e9a46
Matt Zimmer <zimmermja@gmail.com>
2022-01-10 18:58:49 +0000

    set default value of task number to 3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      taskCount: 3,
+      taskCount: 1,
       page: this.pageHome,
     };
   }
--- src/components/setup.js
+++ src/components/setup.js
@@ -5,7 +5,7 @@ class Setup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 1,
+      count: 3,
     };
   }
 
